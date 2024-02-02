44dce0886b0c12963a05ec95e5185757e636ada0
Colin Sung <colinsung0714@gmail.com>
2024-02-02 10:07:29 -0800

    add route

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from route import get_cities_route, get_city_route, create_city, delete_city
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"Hello":"World"}
+
+@app.get('/cities')
+def get_cities():
+    return get_cities_route
+
+@app.get('/cities/{city_id}')
+def get_city(city_id: int):
+    return get_city_route(city_id)
---
+++ route.py
@@ -0,0 +1,26 @@
+import requests
+
+db = []
+
+def get_cities_route():
+    results = []
+    for city in db:
+        strs = "http://worldtimeapi.org/timezone/Asia/Seoul"
+        r = requests.get(strs)
+        cur_time = r.json()['datetime']
+        results.append({'name':city['name']}, {'timezone':city['timezone']}, {'current_time':cur_time})
+    return results
+
+def get_city_route(city_id):
+    city = db[city_id-1]
+    strs= f"http://worldtimeapi.org/timezone/{city['timezone']}"
+    r = requests.get(strs)
+    cur_time = r.json()['datetime']
+    return {'name':city['name'], 'timezone':city['timezone'], 'current_time':cur_time}
+
+# def create_city_route(city_id: int):
+#     db.append(city.dict*())
+#     return db[-1]
+
+# def delete_city(city_id: int):
+#     pass
